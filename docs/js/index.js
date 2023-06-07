70e784491fe543762f8a22b540f22c19a4642168
itsbluejelly <ronniedenzel0@gmail.com>
2023-06-07 23:31:40 +0300

    revised javascript file with features and comments

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,24 +1,55 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const saveBtn = document.getElementById("save-btn")
-const ulEl = document.getElementById("ul-el")
+// GET NECESSARY ELEMENTS
+const saveBtn = document.getElementById("save-btn");
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
+const ulEl = document.getElementById("ul-el");
+const inputEl = document.getElementById("input-el");
 
-saveBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
+// CREATE AN EMPTY LIST TO STORE INPUT VALUES, AND ACCESS LOCAL STORAGE FOR THE STORED LIST
+let inputList = [];
+let storedItems = localStorage.getItem("storedlist");
+
+// IF THE LIST EXISTS IN THE LOCAL STORAGE,THE INPUT LIST BECOMES THE STORED LIST,OTHERWISE IT REMAINS EMPTY
+if(storedItems){
+    inputList = JSON.parse(storedItems);
+    showItems(inputList);
+}
+
+// A FUNCTION TO DISPLAY LIST CONTENTS INSIDE THE UNORDERED LIST VIA JS
+function showItems(arr){
+    let stringedList = "";
+    
+    for (let item of arr){
+        stringedList += `
+                            <li>
+                                <a href='${item}' target="_blank">${item}</a>
+                            </li>
+                        `
+    }
+
+    ulEl.innerHTML = stringedList
+}
+
+// ACTION TAKEN IF ONE CLICKS THE SAVE BUTTON,TO STORE THE INPUT DATA
+saveBtn.addEventListener("click", () => {
+    inputList.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("storedlist", JSON.stringify(inputList));
+    showItems(inputList); 
 })
 
-function renderLeads() {
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += `
-            <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
-                </a>
-            </li>
-        `
-    }
-    ulEl.innerHTML = listItems  
-}
+// ACTION TAKEN IF ONE CLICKS THE DELETE BUTTON,WIPING OUT DATA
+deleteBtn.addEventListener("dblclick", () => {
+    localStorage.clear();
+    inputList = [];
+    showItems(inputList);
+})
+
+// ACTION TO SAVE TAB VISITED
+tabBtn.addEventListener("click", () => {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        inputList.push(tabs[0].url);
+        localStorage.setItem("storedlist", JSON.stringify(inputList));
+        showItems(inputList)
+    })
+})
